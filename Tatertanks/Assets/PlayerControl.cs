d1cd1d5c357697f44ecce7dec323c9e3862e6ed5
Brian <brian.c120@stu.nebo.edu>
2019-10-12 23:57:03 -0600

    Created PlayerControl script nad updated terrain textures

---
+++ Tatertanks/Assets/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 10.0f;
+    public float rotatespeed = 40.0f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move player
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(-Vector3.forward * Time.deltaTime * speed);
+        }
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * rotatespeed);
+        }
+        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down * Time.deltaTime * rotatespeed);
+        }
+    }
+}
